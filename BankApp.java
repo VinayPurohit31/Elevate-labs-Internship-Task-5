64026b8513aeaa93a51687d820c008409edf6aff
VinayPurohit31 <vinaypurohit31@gmail.com>
2025-06-30 17:00:56 +0530

    logic change for the menu now ask for the useid and password first then allow the menu

--- BankApp.java
+++ BankApp.java
@@ -12,66 +12,68 @@ public class BankApp {
 
 class LogicHandling {
 
-    Bank b = new Bank();
+    Bank bank = new Bank();
     Scanner sc = new Scanner(System.in);
 
     void loopLogic() {
+        while (true) {
+            System.out.println("\n---------- Bank Menu ----------");
+            System.out.println("1. Create Account");
+            System.out.println("2. Login to Account");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            int mainChoice = sc.nextInt();
+            sc.nextLine(); 
 
-        try {
-
-            for (;;) {
-
-                System.out.println(" --Select the operation yo want to perform-- ");
-                System.out.println("         1 if you want to create account ");
-                System.out.println("         2 if you want to deposit money ");
-                System.out.println("         3 if you want to withdraw money ");
-                System.out.println("         4 if you want to update account ");
-                System.out.println("         5 if you want to display account  ");
-                System.out.println("         6 if you want to delete account ");
-                System.out.println("         7 if you want to generate statement ");
-                System.out.println("         8 if you want to terminate the session ");
-                System.out.print("         Enter the number :- ");
-                int selectCase = sc.nextInt();
-                System.out.println("");
-                if (selectCase == 8) {
-                    System.out.println("       You terminated the session.");
-                    System.out.println("");
-                    break;
-                }
-                if (selectCase >= 1 && selectCase <= 7) {
-
-                    System.out.println("");
-
-                    switch (selectCase) {
-                        case 1 ->
-                            b.createAccount(sc);
-                        case 2 ->
-                            b.depositMoney();
-                        case 3 ->
-                            b.withdrawMoney();
-                        case 4 ->
-                            b.updateAccount();
-                        case 5 ->
-                            b.displayAccount();
-                        case 6 ->
-                            b.deleteAccount();
-                        case 7 ->
-                            b.generateStatement();
-                        default ->
-                            System.out.println("Illegal value chosen between 1 to 5 ");
+            switch (mainChoice) {
+                case 1 -> bank.createAccount(sc);
+                case 2 -> {
+                    Account loggedInAccount = bank.login(sc);
+                    if (loggedInAccount != null) {
+                        loggedInMenu(loggedInAccount);
+                    } else {
+                        System.out.println("Login failed. Please try again.");
                     }
-                } else if (selectCase < 1 || selectCase > 8) {
-                    System.out.println("      Illegal value. Re-enter the value ");
-                    System.out.println("");
                 }
-
+                case 3 -> {
+                    System.out.println("Exiting the system. Goodbye!");
+                    return;
+                }
+                default -> System.out.println("Invalid choice. Please try again.");
             }
-        } catch (Exception e) {
-            System.out.println("");
-            System.out.println("You entered something illegal, so the whole session was terminated ");
-            System.out.println("");
         }
-        sc.close();
     }
-    
+
+    void loggedInMenu(Account account) {
+        while (true) {
+            System.out.println("\n-- Logged In as: " + account.getUserId() + " --");
+            System.out.println("1. Deposit Money");
+            System.out.println("2. Withdraw Money");
+            System.out.println("3. Update Account");
+            System.out.println("4. Display Account");
+            System.out.println("5. Generate Statement");
+            System.out.println("6. Delete Account");
+            System.out.println("7. Logout");
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1 -> bank.depositMoney(account, sc);
+                case 2 -> bank.withdrawMoney(account, sc);
+                case 3 -> bank.updateAccount(account, sc);
+                case 4 -> bank.displayAccount(account);
+                case 5 -> bank.generateStatement(account);
+                case 6 -> {
+                    bank.deleteAccount(account);
+                    return;
+                }
+                case 7 -> {
+                    System.out.println("Logged out successfully.");
+                    return;
+                }
+                default -> System.out.println("Invalid choice. Try again.");
+            }
+        }
+    }
 }
