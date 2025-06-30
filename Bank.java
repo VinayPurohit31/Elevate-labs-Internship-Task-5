a70f7bea9e79d94b0b713f3357aa16f35debdbfa
VinayPurohit31 <vinaypurohit31@gmail.com>
2025-06-30 11:58:45 +0530

    Empty methods add in the bank class

--- Bank.java
+++ Bank.java
@@ -1,3 +1,25 @@
 public class Bank {
-    
+    public void createAccount(){
+
+    }
+    public void updateAccount(){
+
+    }
+    public void deposicMoney(){
+
+    }
+    public void withdrowMoney(){
+
+    }
+    public void displayAccount(){
+
+    }
+    public void deleteAccount(){
+
+    }
+    public void generateStatement(){
+
+    }
+
+
 }
